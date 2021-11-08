16017a7fddba81d34b81f460ba0080c9441b9e49
dokkalemon <dottori.roberto@gmail..com>
2021-11-08 17:19:48 +0100

    end project

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -4,3 +4,14 @@ A partire da un array di numeri, genera un secondo array con le potenze
 al quadrato di ogni numero.
 Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
 */
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log('numeri normali:', number);
+const quadrato = number.map((number) => {
+    const element = number * number
+
+    return element
+})
+
+console.log('numeri al quadrato', quadrato);
